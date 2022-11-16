07c9e8fc3280a28bfb2b18c2b8e8473f0b4afe3d
HappyDev <67974781+D3R50N@users.noreply.github.com>
2022-11-16 20:39:43 +0100

    Delete b-apk.bat

--- b-apk.bat
+++
@@ -1,9 +0,0 @@
-cls
-set argC=
-for %%x in (%*) do call set argC=%%argC%%%%x 
-if "%argC%"=="" goto commit
-cd "D:\PROGRAMMATION\Flutter\Applications\%argC%" & cls
-D: & cls
-flutter build apk --split-per-abi & cd build\app\outputs\flutter-apk\ &explorer . & echo Ouverture de l'Explorateur
-:commit
-   cls & echo La commande est sous la forme : & echo b-apk Nom du dossier
